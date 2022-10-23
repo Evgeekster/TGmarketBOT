275515d8121290a063341f855f1789dbdd80c5bb
Zhenya <evgenysuch@gmail.com>
2022-10-23 03:06:43 +0300

    updating bd every 60sec

--- BaseDataRedader.py
+++ BaseDataRedader.py
@@ -3,14 +3,22 @@ import sqlite3
 import requests
 import wget
 import urllib.request, json
-with urllib.request.urlopen("https://market.dota2.net/itemdb/current_570.json") as url:
-    data = json.load(url)
-    print(data)
-URL = 'https://market.dota2.net/itemdb/'+data['db']
-PATH = r'C:\Users\evgen\Desktop\PyTGBot\dbholder'
-print(data['db'])
+import time
+while True:
+    with urllib.request.urlopen("https://market.dota2.net/itemdb/current_570.json") as url:
+        data = json.load(url)
+        print(data)
+    URL = 'https://market.dota2.net/itemdb/'+data['db']
+    PATH_TO_BD = r'C:\Users\evgen\Desktop\PyTGBot\dbholder'+data['db']
+    PATH_TO_FOLDER = r'C:\Users\evgen\Desktop\PyTGBot\dbholder'
+    print(data['db'])
+    DB_NAME = data['db']
 
-wget.download(URL)
+    for f in os.listdir(PATH_TO_FOLDER):
+        if not f.endswith(".csv"):
+            continue
+        os.remove(os.path.join(PATH_TO_FOLDER, f))
 
-# with open(os.path.join(PATH,wget.download(URL)), "w") as file1:
-#     file1.write(wget.download(URL))
+    os.chdir(PATH_TO_FOLDER)
+    wget.download(URL)
+    time.sleep(60)
